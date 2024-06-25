ab9a183792582d5fc4d8a81a595c1c97bb190900
James Brooks <brooksjamesk01@gmail.com>
2024-06-25 14:13:48 -0700

    created path_pulse User model

--- path_pulse/models.py
+++ path_pulse/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+class User(models.Model):
+    user_email = models.EmailField(max_length=200)
+    user_trips = ArrayField(
+        ArrayField(
+            models.CharField(max_length=200)
+        )
+    )
+
+    def __str__(self):
+        return self.user_trips
