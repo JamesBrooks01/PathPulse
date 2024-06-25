cff1dce4298f89de6da5f2fa0e6567f8125c52e5
James Brooks <brooksjamesk01@gmail.com>
2024-06-25 15:01:43 -0700

    attempt 4

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,9 @@
-python3.10 -m venv venv
+python3.12 -m venv venv
 
 source venv/bin/activate
 
 pip install -r requirements.txt
 
-python3.10 manage.py migrate 
+python3.12 manage.py migrate 
 
-python3.10 manage.py collectstatic
+python3.12 manage.py collectstatic
