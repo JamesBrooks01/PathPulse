ee9ff2220d7109983f7abc18d94dca8a9ab13fea
James Brooks <brooksjamesk01@gmail.com>
2024-06-26 18:25:08 -0700

    added a DetailView path to the app urls.py and altered the vote path to have a different name

--- path_pulse/urls.py
+++ path_pulse/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'path_pulse'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.VoteView.as_view(), name='detail'),
+    path('<int:pk>/detail', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/', views.VoteView.as_view(), name='trip'),
     path('<int:user_id>/vote', views.vote, name='vote')
 ]
