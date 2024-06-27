0683d70da952c1a2d31500a71000a64f60fb0dfb
James Brooks <brooksjamesk01@gmail.com>
2024-06-26 18:51:19 -0700

    added the url path to the project urls.py for Django Debug Toolbar

--- pulse_site/urls.py
+++ pulse_site/urls.py
@@ -24,5 +24,11 @@ urlpatterns = [
     path('pathpulse/', include('path_pulse.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if not settings.TESTING:
+    urlpatterns = [
+        *urlpatterns,
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
+
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
