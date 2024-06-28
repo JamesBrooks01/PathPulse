af0eb920a9d7060cbee2d703c7d389a6d936fdee
James Brooks <brooksjamesk01@gmail.com>
2024-06-28 16:18:59 -0700

    updated pulse_site urls.py so the index route goes directly to the app urls

--- pulse_site/urls.py
+++ pulse_site/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('path_pulse.urls')),
     path("admin/", admin.site.urls),
-    path('pathpulse/', include('path_pulse.urls')),
 ]
 
 if not settings.TESTING:
