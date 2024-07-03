dc1e17cce6d58c6ded532120dad8a5901a4a7a9b
James Brooks <brooksjamesk01@gmail.com>
2024-07-02 18:02:59 -0700

    added some basic functions for API data grabbing, still in progress

---
+++ utilities_dir/weather_data.py
@@ -0,0 +1,89 @@
+import datetime
+from datetime import date
+import os
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
+import json
+import requests
+
+cache_session = requests_cache.CachedSession('requests_cache', backend='memory', expire_after=3600)
+retry_session = retry(cache_session, retries=5, backoff_factor = 0.2)
+open_metro = openmeteo_requests.Client(session=retry_session)
+
+with open('utilities_dir/weather_codes.json') as weather_file:  
+    weather_codes = json.load(weather_file)
+
+
+def location_data(data):
+    sanitized_spaces = data['location'].replace(' ', '%20')
+    api_key = os.environ.get('LOCATION_API')
+    url = f'https://us1.locationiq.com/v1/search?key={api_key}&q={sanitized_spaces}&format=json'
+    response = requests.get(url)
+    converted = json.loads(response.text)
+    return_dict = {
+        'lat': converted[0]['lat'],
+        'lon': converted[0]['lon']
+    }
+    return return_dict
+
+def weather_data(data):
+    coords = location_data(data)
+    today = date.today()
+    start_unix = datetime.datetime.fromisoformat(data['start_date'])
+    end_unix = datetime.datetime.fromisoformat(data['end_date'])
+    days_from_today = round((start_unix - today) // 86400)
+    if days_from_today < 0:
+        return 'Invalid Date, Start Date is in the Past'
+    boundry = today + (7 * 86400)
+
+    forecast = []
+    historic = []
+    all_weather = {}
+    date_list = []
+
+    marker = start_unix
+    while marker <= end_unix:
+        date_list.append(marker)
+        marker + 86400
+
+    if start_unix >= boundry:
+        historic_weather()
+    elif end_unix >= boundry:
+        both_weather()
+    else:
+        forecast_weather()
+
+
+def historic_weather(lat,lon,start,end):
+    url = "https://historical-forecast-api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "start_date": start,
+        "end_date": end,
+        "daily": ["weather_code", "temperature_2m_max", "temperature_2m_min"],
+        "temperature_unit": "fahrenheit",
+        "wind_speed_unit": "mph",
+        "precipitation_unit": "inch",
+        "timezone": "auto" 
+    }
+
+    response = requests.get(url,params=params)
+    converted = json.loads(response.text)
+    return_data = converted['daily']
+    return_dict = {}
+
+    for x in range(len(return_data['time'])):
+        return_dict[return_data['time'][x]] = {
+            'code': return_data['weather_code'][x],
+            'high': return_data['temperature_2m_max'][x],
+            'low': return_data['temperature_2m_min'][x]
+        }
+    return return_dict
+
+def forecast_weather():
+    pass
+
+def both_weather():
+    pass
