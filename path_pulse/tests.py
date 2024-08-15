18807711019f0bfbe397748fa5eab07ff71f673f
James Brooks <brooksjamesk01@gmail.com>
2024-08-15 11:19:24 -0700

    added inital tests for User Model

--- path_pulse/tests.py
+++ path_pulse/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Trip, User
+
+test_email = 'test_user@email.com'
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(user_email= test_email)
+
+    def test_user_get(self):
+        user = User.objects.get(user_email= test_email)
+        self.assertEqual(str(user), test_email)
+
+
+class TripTestCase(TestCase):
+    pass
