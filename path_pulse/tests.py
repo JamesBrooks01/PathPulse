d4726ab3b2800bbef789303c7fc2cb381349c157
James Brooks <brooksjamesk01@gmail.com>
2024-08-15 17:36:33 -0700

    added basic tests for the index view in the context of a new user first logging in to the site

--- path_pulse/tests.py
+++ path_pulse/tests.py
@@ -47,3 +47,16 @@ class IndexGuestViewTests(TestCase):
         response = self.client.get(reverse('path_pulse:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'You are currently logged in as a guest.')
+
+class IndexInitialUserTests(TestCase):
+    def  setUp(self):
+        session = self.client.session
+        session['user'] = {"userinfo": {"email": test_email,"name": "TestUser","picture": "https://cdn.pixabay.com/photo/2017/11/10/05/46/group-2935521_1280.png"}}
+        session.save()
+
+    def test_index_create_db_user(self):
+        redirect_response = self.client.get(reverse('path_pulse:index'))
+        self.assertRedirects(redirect_response, reverse('path_pulse:index'))
+        response_attempt2 = self.client.get(reverse('path_pulse:index'))
+        self.assertEqual(response_attempt2.status_code, 200)
+        self.assertContains(response_attempt2, 'Trip Location')
